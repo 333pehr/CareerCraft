92b4de73fa22ef1535dd102c29a33916d138fad2
SepehrLatifiAzad <sepehrlatifi.azad@pia-team.com>
2024-10-30 03:12:53 +0300

    structure of the header implemented

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button'; 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, MatButtonToggleModule, MatIconModule, MatMenuModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  selectedLanguage: string = 'ENG';
+  constructor() {}
 
+  onLanguageChanged(language: string) {
+    this.selectedLanguage = language;
+  }
 }
