38ad9aced3cdb90de6ab0b830bf034af6d1b5ff9
SepehrLatifiAzad <sepehrlatifi.azad@pia-team.com>
2024-10-30 16:30:11 +0300

    Translation infrastructure added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,30 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { TranslationLoaderService } from './shared/services/translation-loader.service';
+import { locale as english } from './shared/i18n/en';
+import { locale as turkish } from './shared/i18n/tr';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+
+function appInitial(translationLoader: TranslationLoaderService) {
+  translationLoader.loadTranslations(english, turkish);
+  translationLoader.setDefaultLang(localStorage.getItem('locale') ?? 'en');
+  return () => {};
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appInitial,
+      multi: true,
+      deps: [TranslationLoaderService],
+    },
+    TranslateService,
+    ...(TranslateModule.forRoot() as any).providers,
+  ]
 };
--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button'; 
+import { TranslationLoaderService } from '@app/shared/services/translation-loader.service';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -12,10 +13,36 @@ import {MatButtonModule} from '@angular/material/button';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  selectedLanguage: string = 'ENG';
-  constructor() {}
 
-  onLanguageChanged(language: string) {
+  selectedLanguage: any;
+  languages: any;
+
+  translationLoader = inject(TranslationLoaderService);
+
+  constructor() {
+    this.languages = [
+      {
+          id: 'en',
+          title: 'English',
+          flag: 'us',
+      },
+      {
+          id: 'tr',
+          title: 'Turkish',
+          flag: 'tr',
+      },
+    ];
+
+    const selectedLanguageKey = localStorage.getItem('locale') ?? 'en';
+
+    this.selectedLanguage = this.languages.find((e: any) => e.id === selectedLanguageKey);
+    console.log(this.selectedLanguage);
+  }
+
+  onLanguageChanged(language: any): void {
     this.selectedLanguage = language;
+    this.translationLoader.use(language.id);
+    localStorage.setItem('locale', language.id);
   }
 }
+
